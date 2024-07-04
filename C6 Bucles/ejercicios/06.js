349ff5ca98e265c1b37e6609d83bd6dd0515020d
aldairnunezca <aldairnunezca@gmail.com>
2024-07-04 14:34:13 -0600

    Module 6

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,7 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  var digitos = num.toString()
+  return (digitos.length === 3);
 }
-
 module.exports = tieneTresDigitos;
--- C6 Bucles/ejercicios/21.js
+++ C6 Bucles/ejercicios/21.js
@@ -4,6 +4,9 @@ function esPotenciaDeDos(numero) {
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+  
+  var resultado = Math.log2(numero);
+  return Number.isInteger(resultado);
 }
 
 module.exports = esPotenciaDeDos;
